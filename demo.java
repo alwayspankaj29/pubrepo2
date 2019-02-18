224446517f3df621b2dbda710902ddb00729f765
Pankaj <pankaj.vadnal29@gmail.com>
2019-02-18 23:11:23 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,7 @@
+// Check for arg input
+        if(args.length < 1) {
+            System.out.println("Must specify a term to convert");
+            System.out.println("EXAMPLE:");
+            System.out.println("  java SampleKanaConverter ﾃｽﾄ");
+            return;
+        }
