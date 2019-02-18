4c5df95640885a703fbda78715f5251a4794643c
Pankaj <pankaj.vadnal29@gmail.com>
2019-02-18 23:11:58 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,15 @@
+// Check for arg input
+        if(args.length < 1) {
+            System.out.println("Must specify a term to convert");
+            System.out.println("EXAMPLE:");
+            System.out.println("  java SampleKanaConverter ﾃｽﾄ");
+            return;
+        }
+        
+        // Check for arg field
+        if(args.length < 1) {
+            System.out.println("Must specify a term to convert");
+            System.out.println("EXAMPLE:");
+            System.out.println("  java SampleKanaConverter ﾃｽﾄ");
+            return;
+        }
